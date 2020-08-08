a548deb82c86556559b473f690ff01cce4d618c5
mr_franklin <mr_franklin@126.com>
2020-08-08 17:15:44 +0800

    add dp

--- 0070-ClimbingStairs/solution.cpp
+++ 0070-ClimbingStairs/solution.cpp
@@ -1,7 +1,8 @@
 class Solution {
 public:
     unordered_map<int, int> cache;
-    int climbStairs(int n) {
+    // 递归+cache
+    int climbStairs1(int n) {
         if (n <= 2) {
             return n;
         }
@@ -11,5 +12,15 @@ public:
         int ret = climbStairs(n - 1) + climbStairs(n - 2);
         cache[n] = ret;
         return ret;
+    }
+    // dp
+    int climbStairs(int n) {
+        if (n <= 1) return 1;
+        vector<int> f(n + 1);
+        f[0] = 1, f[1] = 1;
+        for (int i = 2; i <= n; i++) {
+            f[i] = f[i - 1] + f[i - 2];
+        }
+        return f[n];
     }
 };
